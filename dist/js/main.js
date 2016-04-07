51f98ad0394a5c16b2088b4e47c6826747a43ce1
Martin Jay <martinjay180@gmail.com>
2016-04-07 00:12:49 -0400

    Moves game logic from html to game.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -97554,6 +97554,48 @@ Asteroid.prototype = {
         this.sprite.body.bounce = new Phaser.Point(.5, 1);
     }
 }
+var rocket;
+var asteroid;
+var cursors;
+var jumpButton;
+var bg;
+
+var game = new Phaser.Game(1200, 800, Phaser.CANVAS, 'phaser-example', {
+    preload: preload,
+    create: create,
+    update: update,
+    render: render
+});
+
+function preload() {
+    game.stage.disableVisibilityChange = true;
+    game.load.spritesheet('dude', 'img/dude.png', 32, 48);
+    game.load.spritesheet('rocket', 'img/rocket.png', 48, 48);
+    game.load.image('background', 'img/space.jpg');
+    game.load.image('asteroid', 'img/asteroid.png');
+
+}
+
+function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.time.desiredFps = 30;
+    bg = game.add.tileSprite(0, 0, 1200, 800, 'background');
+    game.physics.arcade.gravity.y = 100;
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    rocket = new Rocket(game, cursors, jumpButton);
+    asteroid = new Asteroid(game);
+}
+
+function update() {
+    game.physics.arcade.collide(rocket.sprite, asteroid.sprite);
+    rocket.maneuver();
+}
+
+function render() {
+    game.debug.text(game.time.suggestedFps, 32, 32);
+    game.debug.text(game.time.physicsElapsed, 64, 32);
+}
 function Rocket(game, cursors, jumpButton) {
     this.game = game;
     this.sprite = null;
